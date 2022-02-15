b545d049a7e854013999e50d504b867d764d5df1
Luka <luigi7up@gmail.com>
2022-02-15 14:04:06 +0100

    fixing the asset path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     
-  publicPath: process.env.NODE_ENV === 'production' ? '/newrelic-segment-amplitude/' : '/',  //This is the URL to the assets in github
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',  //This is the URL to the assets in github
 
   devServer: {
     host: 'mylocalhost.com'
